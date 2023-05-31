010063e0b7347838b5380a59f08a0c6a068f1a6d
alexisrdt <alexis.rdt@gmail.com>
2023-06-01 01:25:32 +0200

    Start PNG processing

--- fraus/images.h
+++ fraus/images.h
@@ -1,8 +1,24 @@
 #ifndef FRAUS_IMAGES_H
 #define FRAUS_IMAGES_H
 
+#include <stdint.h>
+
 #include "utils.h"
 
-FrResult frLoadPng(const char* path);
+typedef enum FrImageType
+{
+	FR_RGB,
+	FR_RGBA
+} FrImageType;
+
+typedef struct FrImage
+{
+	uint32_t width;
+	uint32_t height;
+	uint8_t* data;
+	FrImageType type;
+} FrImage;
+
+FrResult frLoadPng(const char* path, FrImage* pImage);
 
 #endif
